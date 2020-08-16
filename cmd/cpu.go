31008ec9007329857e75b2aa5862b3ad281a1ee4
Jeff Hallyburton <jhally@gmail.com>
2020-08-16 15:51:05 -0400

    add cputime, streamline formatting a bit

--- cmd/cpu.go
+++ cmd/cpu.go
@@ -6,31 +6,38 @@ import (
   "github.com/shirou/gopsutil/cpu"
 )
 
-var info bool
+var info    bool
 var percent bool
-var percpu bool
+var cputime bool
+var percpu  bool
 
 func init() {
   cpuinfo.Flags().BoolVarP(&info,    "info",    "i", false, "whether to include cpuinfo in the final result")
   cpuinfo.Flags().BoolVarP(&percent, "percent", "p", false, "whether to include cpu usage percent in final result")
-  cpuinfo.Flags().BoolVarP(&percpu,  "percpu",  "c", false,  "whether to split metrics per cpu core")
+  cpuinfo.Flags().BoolVarP(&cputime, "cputime", "t", false, "whether to include cpu time in the final result")
+  cpuinfo.Flags().BoolVarP(&percpu,  "percpu",  "c", false, "whether to split metrics per cpu core")
   rootCmd.AddCommand(cpuinfo)
 }
 
 func cpuInfo() string {
   info, _ := cpu.Info()
-  json, _ := ConvertToJson(info)
-  return json
+  return format(info)
 }
 
 func cpuPercent() string {
-  info, _ := cpu.Percent(0, percpu)
-  json, _ := ConvertToJson(info)
-  return json
+  percent, _ := cpu.Percent(0, percpu)
+  return format(percent)
 }
 
-//func format(data interface{}) string {
-//}
+func cpuTime() string {
+  time, _ := cpu.Times(percpu)
+  return format(time)
+}
+
+func format(data interface{}) string {
+  json, _ := ConvertToJson(data)
+  return json
+}
 
 var cpuinfo = &cobra.Command {
 
@@ -41,13 +48,21 @@ var cpuinfo = &cobra.Command {
 
   Run: func(cmd *cobra.Command, args []string) {
 
+    var output []string
+
     if info == true {
-      fmt.Print(cpuInfo())
+      output = append(output, cpuInfo())
     }
 
     if percent == true {
-      fmt.Print(cpuPercent())
+      output = append(output, cpuPercent())
     }
+
+    if cputime == true {
+      output = append(output, cpuTime())
+    }
+
+    fmt.Print(output)
 
   },
 }
